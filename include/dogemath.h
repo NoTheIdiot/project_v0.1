a786d94a3e3ed19ab9542b0230cca4a78081078d
NoTheIdiot <m-13175444@moe-dl.edu.my>
2026-05-08 17:23:11 +0000

    such doge such math function

---
+++ include/dogemath.h
@@ -0,0 +1,9 @@
+#ifndef DOGEMATH_H
+#define DOGEMATH_H
+
+int math_power(int input, int power);
+int math_sqaure(int input);
+int math_root(int input, int root);
+int math_sqaureroot(int input);
+
+#endif 
