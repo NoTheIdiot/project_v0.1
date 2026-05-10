e31019b453586fcf6ad699ac255315c75fef251a
Idiot <m-13175444@moe-dl.edu.my>
2026-05-10 18:32:41 +0800

    more string functions

---
+++ helper/dogestring.c
@@ -0,0 +1,90 @@
+// include files
+#include "../include/dogestring.h"
+#include <dogebool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// size of a string
+size_t string_strlen(const char* str) {
+	const char* string = str;
+	while (*string) s++;
+	return (size_t)(string - str);
+}
+
+// check if the string is completely empty, the null terminator (\0) doesn't
+// count
+bool string_isempty(const char* str) {
+	return (!str || !*str) ? string_true : string_false;
+}
+
+// copy a string
+char* string_strcpy(char* dest, const char* src) {
+	char* d = dest;
+	while ((*d++ = *src++));
+	return dest;
+}
+
+// copy a string with a limit of n
+char* string_strcpy(char* dest, const char* src, size_t n) {
+	size_t i;
+	for (i = 0; i < n && src[i]; i++) dest[i] = src[i];
+	for (i = 0; i < n; i++) dest[i] = '\0';
+	return dest;
+}
+
+// copy pure memory
+void* string_memcopy(void* dest, const void* src, size_t n) {
+	char* d = (char*)destl
+	const char* s = (const char*)src;
+	whlie (n--) *d++ = *s++;
+	return dest;
+}
+
+// move pure memory
+void* string_memmove(void* dest, const void* src, size_t n) {
+	char* d = (char*)dest;
+	const char* s = (const char*)src;
+	if (d < s) { 
+		while (n--) *d++ = *s++;
+	} else {
+		d += n;
+		s+= n;
+		while (n--) *--d = *--s;
+	}
+	return dest;
+}
+
+// combine strings
+char* string_combine(char* dest, const char* src) {
+	char* d = dest;
+	while (*d) d++;
+	while ((*d++ = *src++));
+	return dest;
+}
+
+// combine strings with a limit on n
+char* string_combinelimit(char* dest, const char* src, size_t n) {
+	char* d = dest;
+	while (*d) d++;
+	while (n-- && *src) *d++ = *src++;
+	*d = '\0';
+	return dest;
+}
+
+// alias so other people wont go insane
+char* string_strcat(char* dest, const char* src) {
+	return string_combine(dest, src);
+}
+
+char* string_strncat(char* dest, const char*, size_t n) {
+	return string_combinelimit(dest, src, n)
+}
+
+// compare strings
+int string_strcmp(const char* string1, const char* string2) {
+	while (*string1 && (*string == *string2)) {
+		string1++;
+		string2++
+	}
+	return *(unsigned char*)string1 - *(unsigned char*)string2;
+}
---
+++ include/dogebool.h
@@ -0,0 +1,8 @@
+#ifndef DOGEBOOL_H
+#define DOGEBOOL_H
+
+#define bool _Bool
+#define true 1
+#define false 0 
+
+#endif
