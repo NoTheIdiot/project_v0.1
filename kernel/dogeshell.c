356a800675efcb19dddef6f8a416124a895b180c
Idiot <m-13175444@moe-dl.edu.my>
2026-05-17 12:35:14 +0800

    add a simple shell

--- kernel/dogeshell.c
+++ kernel/dogeshell.c
@@ -8,8 +8,12 @@
 #include <multiboot.h>
 #include <time.h>
 
-void doge_shell() {
+extern char* windoge_full_version;
+extern char* windoge_short_version;
+extern void meta_sysinfo();
+extern void meta_dogescript(char* filename);
 
+void doge_shell() {
 	char command[64];
 
 	while (true) {
@@ -17,5 +21,35 @@ void doge_shell() {
 		dogeio_print("windoge-/# ");
 		dogeio_input(command, 64);
 
+		if (string_startsWith(command, "print")) {
+			dogeio_println(command + 6);
+		} else if (string_startsWith(command, "bark")) {
+			dogeio_println(command + 5);
+		}
+
+		if (string_strcmp(command, "ver")) {
+			dogeio_println(windoge_full_version);
+		} else if (string_strcmp(command, "sysinfo")) {
+			meta_sysinfo();
+		}
+
+		if (string_strcmp(command, "time")) {
+			time_update_time();
+			time_show();
+		}
+
+		if (string_strcmp(command, "clear")) {
+			dogeio_clear_screen();
+		}
+
+		if (string_startsWith(command, "dogescript")) {
+			meta_dogescript(command + 10);
+		}
+
+		if (string_strcmp(command, "halt")) {
+			dogeio_clear_screen();
+			dogeio_print("Halting the many cpu, such goodbye!");
+			while (true) { __asm__ volatile ("hlt"); }
+		}
 	}
 }
